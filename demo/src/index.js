bb4ddd2d129cc99a49b5c9f66f9c21b8597d7471
llblh <llblh1016@gmail.com>
2020-06-21 21:27:16 +0800

    deploy 0.2.0

--- demo/src/index.js
+++ demo/src/index.js
@@ -20,25 +20,30 @@ export default class Demo extends Component {
 
   onRun = key => {
     if (key === 'no') {
-      this.state.stackData.reject();
+      this.state.stackData.reject(this.onEndStack);
     } else {
-      this.state.stackData.accept();
+      this.state.stackData.accept(this.onEndStack);
     }
+  }
+
+  onEndStack = event => {
+    console.log(event);
   }
 
   render() {
     return <div className="example">
       <Reactcardstack
         options={{
+          perspective: 500,
           infinite: true,
         }}
         size={{ width: 400, height: 214 }}
         onStack={this.onStack}
       >
-        <div className="example_item1">1</div>
-        <div className="example_item2">2</div>
-        <div className="example_item3">3</div>
-        <div className="example_item4">4</div>
+        <div key="1" className="example_item1">1</div>
+        <div key="2" className="example_item2">2</div>
+        <div key="3" className="example_item3">3</div>
+        <div key="4" className="example_item4">4</div>
       </Reactcardstack>
       <button onClick={() => this.onRun('no')}>no</button>
       <button onClick={() => this.onRun('yes')}>yes</button>
